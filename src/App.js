fbd042c87eb2d70e26caaf9ffaf258fbf15619b6
heaven.venorg <heavensipangkar1@gmail.com>
2024-12-05 12:04:04 +0000

    #

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import "./App.css";
 // Note Navbar Style in "./App.css"
 import Navbar from "./component/Navbar";
 import Landing from "./component/Landing/Landing";
-import Celtofah from "./component/Celtofah/Celtofah";
 import Fahtocel from "./component/Fahtocel/Fahtocel";
+import Celtofah from "./component/Celtofah/Celtofah";
 
 const App = () => {
   return (
